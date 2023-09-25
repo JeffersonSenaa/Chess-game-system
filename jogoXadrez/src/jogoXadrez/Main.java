4050b5e86c0fd4b52b8ee51ca8d2aaeee2537f78
JeffersonSenaa <jeffersonsena12144@gmail.com>
2023-09-25 13:45:45 -0300

    Criação da classe posição.

---
+++ jogoXadrez/src/jogoXadrez/Main.java
@@ -0,0 +1,13 @@
+package jogoXadrez;
+
+import tabela.Posicao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
