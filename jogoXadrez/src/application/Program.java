5aebee2bb52814dc4aab2f07be55c125c12a50df
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-01-25 11:25:40 -0300

    Inserção de chessException e lógica para instanciar posições do tabuleiro. Melhorias no UI da partida

--- jogoXadrez/src/application/Program.java
+++ jogoXadrez/src/application/Program.java
@@ -1,8 +1,8 @@
-package chessgame;
+package application;
 
 import chess.ChessMath;
 
-public class Main {
+public class Program {
 
 	public static void main(String[] args) {
 		ChessMath chessMath = new ChessMath();
