4c60ea09bc584ffde79a46c3dde3811b3309e065
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-01-29 10:59:26 -0300

    Adicionando Lógica de xeque

--- jogoXadrez/src/chess/ChessPiece.java
+++ jogoXadrez/src/chess/ChessPiece.java
@@ -16,6 +16,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPosition(position);
+    }
+
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece opponent = (ChessPiece)getBoard().piece(position);
         return opponent !=null & opponent.getColor() != color;
